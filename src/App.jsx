8779f049ba96898deab5a852c0178eb48c4ec68f
Anil Kumar <72590342+diablo3775@users.noreply.github.com>
2023-01-17 10:03:46 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+
+export default function App() {
+  const [sliderValue, setSliderValue] = useState(0);
+  const [emoji, setEmoji] = useState('😀');
+
+  useEffect(() => {
+    updateEmoji();
+  }, [sliderValue]);
+
+  function updateEmoji() {
+    if (sliderValue < 25) {
+      setEmoji('😀');
+    } else if (sliderValue < 50) {
+      setEmoji('😃');
+    } else if (sliderValue < 75) {
+      setEmoji('😎');
+    } else {
+      setEmoji('😴');
+    }
+  }
+
+  return (
+    <div>
+      <input
+        type="range"
+        min={0}
+        max={100}
+        value={sliderValue}
+        onChange={e => setSliderValue(e.target.value)}
+      />
+      <p type="range">{emoji}</p>
+    </div>
+  );
+}
